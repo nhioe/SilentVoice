bdf620bf7707a9a679e0868df456d8bdccfbc124
stwig <S_kw888@yahoo.ca>
2024-09-15 02:54:58 -0400

    uwuwuw

---
+++ my-app/backend/examples.py
@@ -0,0 +1,20 @@
+import cohere
+from cohere import ClassifyExample
+
+examples = [
+    ClassifyExample(text="I Dont know what's going on", label="NoConf"),
+    ClassifyExample(text="I'm not sure what to do", label="NoConf"),
+    ClassifyExample(
+        text="I am going and going and going", label="NoConf"),
+    ClassifyExample(text="Hello! My name is John.", label="Conf"),
+    ClassifyExample(text="I am going to the place and teaching", label="NoConf"),
+    ClassifyExample(text="Your parcel will be delivered today",
+                    label="Conf"),
+    ClassifyExample(
+        text="Review changes to our Terms and Conditions", label="Conf"
+    ),
+    ClassifyExample(text="Weekly sync notes", label="Conf"),
+    ClassifyExample(text="We desire to be loved",
+                    label="Conf"),
+    ClassifyExample(text="One two three four", label="Conf"),
+]
